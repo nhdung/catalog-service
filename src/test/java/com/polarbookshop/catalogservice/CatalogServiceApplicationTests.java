61855df32226b69dff794de546415b32c0111a11
dung.nguyen <dung.nguyen@vidiva.vn>
2025-07-04 16:06:24 +0700

    implement rest api

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,14 +1,95 @@
 package com.polarbookshop.catalogservice;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @BeforeEach
+    void cleanDatabase() {
+        bookRepository.deleteAll();
+    }
+
     @Test
-    void contextLoads() {}
+    void whenGetRequestWithIsbnThenBookReturned() {
+        var bookIsbn = "1234567890";
+        var bookToCreate = Book.builder()
+                .isbn(bookIsbn)
+                .title("Title")
+                .author("Author")
+                .price(9.90)
+                .publisher("Polarsophia")
+                .build();
+
+        Book expectedBook = webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+                .returnResult().getResponseBody();
+
+        webTestClient
+                .get()
+                .uri("/books/" + bookIsbn)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Book.class)
+                .value(
+                        actualBook -> {
+                            assertThat(actualBook).isNotNull();
+                            assertThat(actualBook.getIsbn()).isEqualTo(expectedBook.getIsbn());
+                        });
+    }
+
+    @Test
+    void whenDeleteRequestThenBookDeleted() {
+        var bookIsbn = "1231231233";
+        var bookToCreate = Book.builder()
+                .isbn(bookIsbn)
+                .title("Title")
+                .author("Author")
+                .price(9.90)
+                .publisher("Polarsophia")
+                .build();
+
+        webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated();
+
+        webTestClient
+                .delete()
+                .uri("/books/" + bookIsbn)
+                .exchange()
+                .expectStatus().isNoContent();
+
+        webTestClient
+                .get()
+                .uri("/books/" + bookIsbn)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(String.class).value(errorMessage -> assertThat(errorMessage)
+                        .isEqualTo("The book with ISBN " + bookIsbn + " was not found."));
+    }
 
 }
